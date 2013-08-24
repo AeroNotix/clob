a39004f21d849ea6e108272cfd2248783ff95f4a
Aaron <aaron.l.france@gmail.com>
2013-08-24 17:31:36 +0200

    Whitespace.

--- handlers.lisp
+++ handlers.lisp
@@ -1,6 +1,5 @@
 (in-package :clob-handlers)
 
-
 (defun start ()
   (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242)))
 
@@ -12,3 +11,4 @@
   (setf (hunchentoot:content-type*) "text/html")
   "<html><h3>Not implemented</h3></html>")
 
+(hunchentoot:define-easy-handler (bloglost :uri "/blog/:blogname/"
