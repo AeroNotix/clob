536e9870d4f83ddb828ab88219ddf646a233f35c
Aaron <aaron.l.france@gmail.com>
2013-08-24 23:26:11 +0200

    Finally understood the dev-view-class syntax enough to make it work.

--- handlers.lisp
+++ handlers.lisp
@@ -1,7 +1,37 @@
 (in-package :clob-handlers)
 
+(clsql:locally-enable-sql-reader-syntax)
+
 (defun start ()
   (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242)))
+
+(clsql-sys:def-view-class |blog-entry| ()
+  ((id
+    :type integer
+    :column |id|
+    :db-kind :key
+    :db-constraints :not-null
+    :initarg :id)
+   (date
+    :db-type "date"
+    :column |date|
+    :db-constraints :not-null
+    :initarg :date)
+   (blog-url
+    :type (string 64)
+    :column |blog_url|
+    :db-constraints :not-null
+    :initarg :url)
+   (blog-title
+    :type (string 64)
+    :column |blog_title|
+    :db-constraints :not-null
+    :initarg :url)
+   (blog-post
+    :db-type "longtext"
+    :column |blog_post|
+    :db-constraints :not-null
+    :initarg :post)))
 
 (defmacro with-db (database &body body)
   `(clsql:with-database (,database *connection-details* :database-type *database-type*)
