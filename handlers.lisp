c263b548f629db32f852c0a13d3e58814238bd15
Aaron <aaron.l.france@gmail.com>
2013-08-24 17:15:11 +0200

    Renamed say-yo to index.

--- handlers.lisp
+++ handlers.lisp
@@ -4,6 +4,6 @@
 (defun start ()
   (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242)))
 
-(hunchentoot:define-easy-handler (say-yo :uri "/") ()
+(hunchentoot:define-easy-handler (index :uri "/") ()
   (setf (hunchentoot:content-type*) "text/html")
   "<html><h3>Not implemented</h3></html>")
