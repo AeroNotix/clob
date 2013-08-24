efb2438a556d49bd74eb537e2ac2565343c871af
Aaron <aaron.l.france@gmail.com>
2013-08-24 22:34:29 +0200

    Created a with-db macro to wrap all database calls with so we can easily set up and tear down new database connections.

--- handlers.lisp
+++ handlers.lisp
@@ -3,6 +3,9 @@
 (defun start ()
   (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242)))
 
+(defmacro with-db (database &body body)
+  `(clsql:with-database (,database *connection-details* :database-type *database-type*)
+     ,@body))
 (hunchentoot:define-easy-handler (index :uri "/") ()
   (setf (hunchentoot:content-type*) "text/html")
   "<html><h3>Not implemented</h3></html>")
