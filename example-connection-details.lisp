2c56935f93f11f1ec1bad102111ea142c6b1573e
Aaron <aaron.l.france@gmail.com>
2013-08-24 19:45:32 +0200

    Added a *database-type* to hold the database-type for the clsql database type parameter.

--- example-connection-details.lisp
+++ example-connection-details.lisp
@@ -1,4 +1,5 @@
 (in-package :clob-connection-details)
 
 (defparameter *connection-details* '("host" "table" "user" "password"))
+(defparameter *database-type* :database-type) ;;; Such as :postgresl or :oracle
 
