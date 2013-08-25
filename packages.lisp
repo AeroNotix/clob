0ffc2183a2e425447879fb99e34798bcada9fb45
Aaron <aaron.l.france@gmail.com>
2013-08-25 21:30:20 +0200

    Added the exports for the clob-template package.

--- packages.lisp
+++ packages.lisp
@@ -1,5 +1,10 @@
 (in-package :cl-user)
 
+
+(defpackage #:clob-templates
+  (:use #:cl #:djula)
+  (:export :render-blog :blog :not-found :blog-mode))
+
 (defpackage #:clob-connection-details
   (:use #:cl)
   (:export :*connection-details* :*database-type*))
